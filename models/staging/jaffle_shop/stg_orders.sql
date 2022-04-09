7d1222bfdd2b7c2ec50bfcaed19edda7dde19608
kocha017@gmail.com <kocha017@gmail.com>
2022-04-09 15:43:51 +0000

    Updated schemas

---
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -0,0 +1,7 @@
+select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    "STATUS"
+
+from training_db_dave_k.jaffle_shop.orders
---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,9 @@
+SELECT
+    ID,
+    ORDERID,
+    PAYMENTMETHOD,
+    "STATUS",
+    AMOUNT,
+    CREATED,
+    _BATCHED_AT
+FROM training_db_dave_k.STRIPE.PAYMENT
