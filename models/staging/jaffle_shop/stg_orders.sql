b1895dec4ac56dceebd2a4307628c98a3fc84721
kocha017@gmail.com <kocha017@gmail.com>
2022-04-14 00:53:23 +0000

    Added source yml

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,7 +1,6 @@
 select
-    id as order_id,
+    id      as order_id,
     user_id as customer_id,
     order_date,
     "STATUS"
-
-from training_db_dave_k.jaffle_shop.orders
+from {{ source('jaffle_shop', 'orders') }}
